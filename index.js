676612f0da8a258061da4f08db40c745709f3b36
Just¡n <161922331+justinpongos@users.noreply.github.com>
2024-03-16 20:05:38 -0700

    Add files via upload
    Home Page Main

---
+++ index.js
@@ -0,0 +1,10 @@
+//let is how you instantiate a variable
+//type of returns the type of var
+//js is to print in the console log
+
+let firstName = "Justin";
+let lastName = "Pongos";
+let age = 19;
+
+document.getElementById("myH1").textContent = "Hello";
+//document.getElementById("myP").textContent = "This is a description";
