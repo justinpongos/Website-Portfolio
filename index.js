31127b0027c88d97dee7bd907f3f90df11eb5390
Just¡n <161922331+justinpongos@users.noreply.github.com>
2024-03-17 01:02:20 -0700

    Add files via upload
    Finished Education Tab, Working on Skills

--- index.js
+++ index.js
@@ -2,9 +2,3 @@
 //type of returns the type of var
 //js is to print in the console log
 
-let firstName = "Justin";
-let lastName = "Pongos";
-let age = 19;
-
-document.getElementById("myH1").textContent = "Hello";
-//document.getElementById("myP").textContent = "This is a description";
