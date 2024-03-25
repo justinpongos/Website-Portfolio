bea8220b37c60425c614da2b1f901c4184e3afcc
Just¡n <161922331+justinpongos@users.noreply.github.com>
2024-03-25 00:24:45 -0700

    Add files via upload
    progress skills basically done

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 //type of returns the type of var
 //js is to print in the console log
 
-let firstName = "Justin";
-let lastName = "Pongos";
-let age = 19;
+// cursor trails
+window.addEventListener('mousemove', function(e) {
+    var arr = [1, 0.9, 0.8, 0.5, 0.2];
 
-document.getElementById("myH1").textContent = "Hello";
-//document.getElementById("myP").textContent = "This is a description";
+    arr.forEach(function(i) {
+        var x = (1 - i) * 75;
+        var star = document.createElement('div');
+
+        star.className = 'star';
+        star.style.top = e.pageY + Math.round(Math.random() * x - x / 2) + 'px';
+        star.style.left = e.pageX + Math.round(Math.random() * x - x / 2) + 'px';
+
+        document.body.appendChild(star);
+
+        window.setTimeout(function() {
+        document.body.removeChild(star);
+        }, Math.round(Math.random() * i * 600));
+    });
+}, false);
